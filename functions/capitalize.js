5a1c3648b300600bc71ed8f57ea1cff7dad39522
Rob <rverplanke@gmail.com>
2024-03-16 22:11:13 +0100

    finish capitalize function and tests

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,7 +1,12 @@
 function capitalize(value) {
     if (!value) return '';
+    if (typeof value !== 'string' || value[0] === ' ') return 'Not a valid string';
 
-    const capital = value[0].toUpperCase(); // Get first character and capitalize it
+    const firstChar = value[0]; // Get first character
+    
+    if (!(/[a-zA-Z]/.test(firstChar))) return 'No first letter'; // Check if first character is a letter
+
+    const capital = firstChar.toUpperCase(); // Capitalize first character
     const result = capital.concat(value.slice(1)); // Remove first character of string and add it after the capital
 
     return result;
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -9,23 +9,54 @@ describe('capitalize', () => {
     const result = capitalize();
     expect(typeof result).toBe('string');
   })
+  
+  it('should return Not a valid string message if input is not a string', () => {
+    const input = 123;
+    expect(capitalize(input)).toBe('Not a valid string');
+  })
+  
+  it('should return empty string if input is empty', () => {
+    const input = '';
+    expect(capitalize(input)).toBe('');
+  })
 
-  it('should capitalize first character', () => {
+  it('should return empty string if input begins with a special character', () => {
+    const input = '@#$%';
+    expect(capitalize(input)).toBe('No first letter');
+  })
+  
+  it('should return No first letter message if first character is not a letter', () => {
+    const input = '123' ;
+    expect(capitalize(input)).toBe('No first letter');
+  })
+
+  it('should capitalize first character of given string', () => {
     const input = 'example';
     const result = capitalize(input);
     expect(result).toBe('Example');
   })
 
-  it('should capitalize first character of every given string', () => {
-  const input = 'flexible';
-  const result = capitalize(input);
-  expect(result).toBe('Flexible');
+  it('should capitalize first character of any given string', () => {
+    const input = 'flexible';
+    const result = capitalize(input);
+    expect(result).toBe('Flexible');
+  })
+
+  it('should not change input if it exists of a single capital letter', () => {
+    const input = 'A';
+    expect(capitalize(input)).toBe('A');
+  })
+
+  it('should return capitalised string that is already capitalized', () => {
+    const input = 'Capitalized';
+    const result = capitalize(input);
+    expect(result).toBe('Capitalized');
   })
 
   it('should handle multiple words', () => {
-    const input = 'this is a sentence';
+    const input = 'this is a sentence.';
     const result = capitalize(input);
-    expect(result).toBe('This is a sentence');
+    expect(result).toBe('This is a sentence.');
     })
-
 });
+
