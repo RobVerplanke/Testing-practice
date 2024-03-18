550e8ee52814115268e4cf1988b6bc6716e8bcb3
Rob <rverplanke@gmail.com>
2024-03-18 17:26:03 +0100

    finish calculator function and tests

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,22 +1,43 @@
 class Calculator {
-    constructor(valueOne, valueTwo) {
-
+    constructor() {
     }
-        add() {
-            return '';
-        }
 
-        substract() {
-            return '';
-        }
+    // Check for valid input first
+    validateInput(valueOne, valueTwo){
+        if (typeof valueOne !== 'number' || typeof valueTwo !== 'number' ||
+        valueOne === '' || valueTwo === '') {
+        return false; // At least one of the arguments is not a number or empty
+    }
+        return true; // Both values are numbers
+    }
 
-        divide() {
-            return '';
+    add(valueOne, valueTwo) {
+        if (this.validateInput(valueOne, valueTwo)){
+            return valueOne + valueTwo; // Input is valid, add values
         }
+        return 'NaN'; // Input is not valid
+    }
 
-        multiply() {
-            return '';
+    substract(valueOne, valueTwo) {
+        if (this.validateInput(valueOne, valueTwo)){ 
+          return valueOne - valueTwo; // Input is valid, substract values
         }
+        return 'NaN'; // Input is not valid
+    }
+
+    divide(valueOne, valueTwo) {
+        if (this.validateInput(valueOne, valueTwo)){
+            return valueOne / valueTwo; // Input is valid, divide values
+        }
+        return 'NaN'; // Input is not valid
+    }
+
+    multiply(valueOne, valueTwo) {
+        if (this.validateInput(valueOne, valueTwo)){
+            return valueOne * valueTwo; // Input is valid, multiply values
+        }
+        return 'NaN'; // Input is not valid
+    }
 }
 
 const calculator = new Calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -6,22 +6,78 @@ describe('calculator', () => {
         expect(calculator).toBeDefined();
     })
 
-    it('should have a method called add', () => {
+    it('should have the methods add, substract, divide and multiply', () => {
         expect(calculator.add()).toBeDefined();
-    })
-    it('should have a method called substract', () => {
         expect(calculator.substract()).toBeDefined();
-    }) 
-    
-    it('should have a method called divide', () => {
         expect(calculator.divide()).toBeDefined();
-    }) 
-    
-    it('should have a method called multiply', () => {
         expect(calculator.multiply()).toBeDefined();
     })
 
-    // it('', () => {
-    //     expect(calculator).toBe();
-    // })
+    it('should run validateInput for each other method', () => {
+        const input = 'test';
+        expect(calculator.add(input)).toBe('NaN');
+        expect(calculator.substract(input)).toBe('NaN');
+        expect(calculator.divide(input)).toBe('NaN');
+        expect(calculator.multiply(input)).toBe('NaN');
+    })
+
+    it('should return NaN when arguments are not numbers', () => {
+        const inputOne = 'One';
+        const inputTwo = 'Two';
+        expect(calculator.add(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.substract(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.divide(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.multiply(inputOne, inputTwo)).toBe('NaN');
+    })
+
+    it('should return NaN when one of the arguments is not a number', () => {
+        const inputOne = 1;
+        const inputTwo = 'Two';
+        expect(calculator.add(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.substract(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.divide(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.multiply(inputOne, inputTwo)).toBe('NaN');
+    })
+
+    it('should return NaN when one of the arguments is empty', () => {
+        const inputOne = '';
+        const inputTwo = 'Two';
+        expect(calculator.add(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.substract(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.divide(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.multiply(inputOne, inputTwo)).toBe('NaN');
+    })
+
+    it('should return NaN when input is empty', () => {
+        const inputOne = '';
+        const inputTwo = '';
+        expect(calculator.add(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.substract(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.divide(inputOne, inputTwo)).toBe('NaN');
+        expect(calculator.multiply(inputOne, inputTwo)).toBe('NaN');
+    })
+
+    it('should add two numbers', () => {
+        const inputOne = 1;
+        const inputTwo = 2;
+        expect(calculator.add(inputOne, inputTwo)).toBe(3);
+    })
+
+    it('should substract two numbers', () => {
+        const inputOne = 3;
+        const inputTwo = 2;
+        expect(calculator.substract(inputOne, inputTwo)).toBe(1);
+    })
+
+    it('should divide two numbers', () => {
+        const inputOne = 8;
+        const inputTwo = 2;
+        expect(calculator.divide(inputOne, inputTwo)).toBe(4);
+    })
+
+    it('should multiply two numbers', () => {
+        const inputOne = 2;
+        const inputTwo = 3;
+        expect(calculator.multiply(inputOne, inputTwo)).toBe(6);
+    })
 })
